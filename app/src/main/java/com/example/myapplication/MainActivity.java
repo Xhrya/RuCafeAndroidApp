669cea579aa207cb9f5d4989fc8ce67393f39ac2
Shreya Pandey <42945783+Xhrya@users.noreply.github.com>
2023-04-17 17:12:01 -0400

    changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnOpenAct2);
+        button.setOnClickListener(new View.OnClickListener())
+        {
+            @Override
+                    public void onClick(View v)
+            {
+                Intent intent = new Intent(this, donutsViewActivity.class );
+                startActivity(intent);
+            }
     }
 
     @Override
@@ -22,5 +36,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void ordersCoffee(View view) {
         Toast.makeText(this, "Welcome to ordering coffee!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, coffeeViewActivity.class);
+        startActivity(intent);
     }
+
+    public void ordersDonut(View view2)
+    {
+
+        Toast.makeText(this, "Welcome to ordering donuts!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, donutsViewActivity.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/donutsViewActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class donutsViewActivity extends AppCompatActivity {
+
+    Intent intent = getIntent();
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donuts_view);
+        button = findViewById(R.id.btnOpenMain);
+        button.setOnClickListener(new View.onClickListener());
+        {
+            @Override
+            public void onClick (View v)
+            {
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+            }
+        }
+    }
+
+}
